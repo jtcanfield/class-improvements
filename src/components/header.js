d4f36bd63434502b5561da1b99c70badf7937cc8
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-18 17:17:20 -0500

    add favicon

--- src/components/header.js
+++ src/components/header.js
@@ -12,24 +12,29 @@ class Header extends Component {
   render() {
     return (
       <div className="Header">
-        <NavLink className="verti-center" to="/">
-          <input className='btn btn-link font-size-25px' type='submit' value='Home'/>
-        </NavLink>
-        <NavLink className="verti-center" to="/about">
-          <input className='btn btn-link font-size-25px' type='submit' value='About'/>
-        </NavLink>
-        <NavLink className="verti-center" to="/news">
-          <input className='btn btn-link font-size-25px' type='submit' value='News'/>
-        </NavLink>
-        <NavLink className="verti-center" to="/contact">
-          <input className='btn btn-link font-size-25px' type='submit' value='Contact'/>
-        </NavLink>
-        <NavLink className="verti-center" to="/events">
-          <input className='btn btn-link font-size-25px' type='submit' value='Events'/>
-        </NavLink>
-        <NavLink className="verti-center" to="/iwoc">
-          <input className='btn btn-link font-size-25px' type='submit' value='IWOC'/>
-        </NavLink>
+        <div>
+          <h3>Raleigh-Durham Industrial Workers of the World</h3>
+        </div>
+        <div className="navlinks">
+          <NavLink className="verti-center" to="/">
+            <input className='btn btn-link font-size-25px' type='submit' value='Home'/>
+          </NavLink>
+          <NavLink className="verti-center" to="/about">
+            <input className='btn btn-link font-size-25px' type='submit' value='About'/>
+          </NavLink>
+          <NavLink className="verti-center" to="/news">
+            <input className='btn btn-link font-size-25px' type='submit' value='News'/>
+          </NavLink>
+          <NavLink className="verti-center" to="/contact">
+            <input className='btn btn-link font-size-25px' type='submit' value='Contact'/>
+          </NavLink>
+          <NavLink className="verti-center" to="/events">
+            <input className='btn btn-link font-size-25px' type='submit' value='Events'/>
+          </NavLink>
+          <NavLink className="verti-center" to="/iwoc">
+            <input className='btn btn-link font-size-25px' type='submit' value='IWOC'/>
+          </NavLink>
+        </div>
       </div>
     );
   }
