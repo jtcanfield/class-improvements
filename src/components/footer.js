dcba43e0bc82663aa5fe147d8910cde9afa2af04
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-18 15:53:37 -0500

    add header and footer

---
+++ src/components/footer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Footer extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="Footer">
+        <h1>FOOTER</h1>
+      </div>
+    );
+  }
+}
+
+export default Footer;
---
+++ src/components/header.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    };
+  }
+
+  render() {
+    return (
+      <div className="Header">
+        <h1>HEADER</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
