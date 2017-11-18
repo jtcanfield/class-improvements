bcd7f2d8a5c3cf2f542782720c49385e34b4180d
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-18 17:02:31 -0500

    add events

--- src/components/Events.js
+++ src/components/Events.js
@@ -5,19 +5,11 @@ class Events extends Component {
   constructor(props) {
     super(props);
     this.state = { width: '0', height: '0' };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
+  componentWillMount(){
     this.setState({ width: window.innerWidth, height: window.innerHeight });
   }
   componentDidMount(){
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
     request
       .get(`graph.facebook.com/v2.11/1587504814641018`)
       .set({"access_token": "196378587576073"})
@@ -25,21 +17,25 @@ class Events extends Component {
        })
   }
   render() {
-    let styles = {
+    let eventsstyles = {
+      "margin":"0 auto",
+      "textAlign":"center",
+    }
+    let iframelink = `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftriangleiww%2Fevents%2F&tabs=events&width=500&height=1000&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=false&appId=196378587576073`
+    let iframestyles = {
       "width":`500px`,
       "height":`1000px`,
       "border":"none",
       "overflow":"hidden",
-      "scrolling":"yes",
+      "scrolling":"no",
       "frameborder":"0",
       "allowTransparency":"true",
     }
-    let iframelink = `https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2Ftriangleiww%2Fevents%2F&tabs=timeline&width=500&height=1000&small_header=true&adapt_container_width=false&hide_cover=false&show_facepile=false&appId=196378587576073`
     return (
-      <div className="Events">
-        <h1>Events</h1>
+      <div className="Events" style={eventsstyles}>
+        <h1>Upcomming Events</h1>
         <iframe src={iframelink}
-        style={styles}></iframe>
+        style={iframestyles}></iframe>
       </div>
     );
   }
