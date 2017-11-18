70d9ad32dcdc703f39ab59ec79c1ed9c2a10b8a7
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-18 17:45:30 -0500

    merge contact and join pages

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -3,10 +3,24 @@ import React, { Component } from 'react';
 class Contact extends Component {
   constructor(props) {
     super(props);
-    this.state = { width: '0', height: '0' };
+    this.state = {
+      width: '0',
+      height: '0',
+      email: "",
+      phone: "",
+    };
+    this.submitform=this.submitform.bind(this);
   }
   componentWillMount(){
     this.setState({ width: window.innerWidth, height: window.innerHeight });
+  }
+  updateFromField(stateKey) {
+    return (event) => {
+      this.setState({[stateKey]: event.target.value});
+    }
+  }
+  submitform(event){
+    // event.preventDefault();
   }
   render() {
     let eventsstyles = {
@@ -26,6 +40,26 @@ class Contact extends Component {
     return (
       <div className="Contact" style={eventsstyles}>
         <h1>Contact Us</h1>
+        <form className="enterForm" onSubmit={this.handleFormSubmit}>
+          <div className="form-group">
+            <h6>Email:</h6>
+            <input type="email" onChange={this.updateFromField('email')}
+            value={this.state.email}
+            placeholder="email"
+            required="true"/>
+          </div>
+          <div className="form-group">
+            <h6>Phone:</h6>
+            <input type="tel" onChange={this.updateFromField('phone')}
+            value={this.state.phone}
+            placeholder="phone"
+            required="true"/>
+          </div>
+          {this.state.message ? this.state.message : ""}<br/>
+          <div className="form-group pull-right">
+            <button className="btn btn-primary btn-lg" type="submit" onClick={event => this.submitform(event)}>Submit</button>
+          </div>
+        </form>
         <iframe title="events" src={iframelink}
         style={iframestyles}></iframe>
       </div>
