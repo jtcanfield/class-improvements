da690595e9fb2a3ce7a2f4ac8442703d6ad7ca02
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-20 16:12:11 -0500

    add GDC description

--- src/components/Gdc.js
+++ src/components/Gdc.js
@@ -4,7 +4,30 @@ class Gdc extends Component {
   render() {
     return (
       <div className="Gdc">
-        <h4 className="leftaligned">General Defence Committee</h4>
+        <h4 className="leftaligned">General Defense Committee</h4>
+        <p>The Industrial Workers of the World (IWW)'s General Defense Committee (GDC) was formed to defend working class people fighting for their rights on the picket line and in their community.</p>
+        <div className="leftaligned">
+          <p>The GDC aims include:</p>
+          <ul>
+            <li>Providing support to any member of the working class who finds themselves in legal trouble due to their involvement in the class war.</li>
+            <li>Organizing against forces of oppression that divide the working class, including colonialism, fascism, heterosexism, racism, patriarchy, state repression, transphobia, and xenophobia, among others.</li>
+            <li>Building networks for community-based self-defense.</li>
+          </ul>
+          <p>The Raleigh-Durham GDC's projects currently include:</p>
+          <ul>
+            <li>Organizing to counter the resurgent movements of white supremacists, neo-Confederates, and the  'alt-right'</li>
+            <li>Community-based food distribution (with Workers Feeding Workers)</li>
+            <li>Campaigns for building renters' power against landlords</li>
+            <li>Organizing to defend people from state and right-wing repression</li>
+          </ul>
+        </div>
+        <p>
+        Are you interested in getting involved with the GDC?<br/>
+        Have ideas for how you'd like to contribute or collaborate?
+        <br/>
+        Contact us at
+        rdiwwgmb@gmail.com
+        </p>
       </div>
     );
   }
--- src/components/Posters.js
+++ src/components/Posters.js
@@ -10,10 +10,10 @@ class Posters extends Component {
       <div className="Posters">
         <h4 className="leftaligned">Posters</h4>
         <div className="poster_holder">
-          <h5>General Defence Comittee Flyer</h5>
-          <img alt="General Defence Comittee Flyer" src="./gdcflyer.jpg" style={posters}/>
+          <h5>General Defense Comittee Flyer</h5>
+          <img alt="General Defense Comittee Flyer" src="./gdcflyer.jpg" style={posters}/>
           <h5>Anti-Identity Evropa Flyer</h5>
-          <img alt="Community Defence Poster" src="./iepostercolor.jpg" style={posters}/>
+          <img alt="Community Defense Poster" src="./iepostercolor.jpg" style={posters}/>
           <h5>Anti-Nazi Poster</h5>
           <img alt="Anti-Nazi Poster" src="./iwwantinaziposter.jpg" style={posters}/>
         </div>
