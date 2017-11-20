68ef4f23c06eefd140b15b5968f40d8c020dc916
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-20 18:25:06 -0500

    cented links and made them grey

--- src/components/Posters.js
+++ src/components/Posters.js
@@ -10,11 +10,11 @@ class Posters extends Component {
       <div className="Posters">
         <h4 className="leftaligned">Posters</h4>
         <div className="poster_holder">
-          <a href="./gdcflyer.jpg" target="blank"><h5 className="leftaligned">General Defense Comittee Flyer</h5>
+          <a href="./gdcflyer.jpg" target="blank"><h5>General Defense Comittee Flyer</h5>
           <img alt="General Defense Comittee Flyer" src="./gdcflyer.jpg" style={posters}/></a>
-          <a href="./iepostercolor.jpg" target="blank"><h5 className="leftaligned">Anti-Identity Evropa Flyer</h5>
+          <a href="./iepostercolor.jpg" target="blank"><h5>Anti-Identity Evropa Flyer</h5>
           <img alt="Community Defense Poster" src="./iepostercolor.jpg" style={posters}/></a>
-          <a href="./iwwantinaziposter.jpg" target="blank"><h5 className="leftaligned">Anti-Nazi Poster</h5>
+          <a href="./iwwantinaziposter.jpg" target="blank"><h5>Anti-Nazi Poster</h5>
           <img alt="Anti-Nazi Poster" src="./iwwantinaziposter.jpg" style={posters}/></a>
         </div>
       </div>
