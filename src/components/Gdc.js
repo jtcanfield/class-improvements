aef8fe3e8876921b62dddf6a147ed875f440681b
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-11-20 19:41:20 -0500

    add more pic styles

--- src/components/Gdc.js
+++ src/components/Gdc.js
@@ -10,26 +10,37 @@ class Gdc extends Component {
     let gdclogo = {
       "maxHeight":"250px",
     }
+    let blocked = {
+      "display":"block",
+    }
     return (
       <div className="Gdc">
         <img alt="General Defense Comittee Holding Anti-Klan Banner" style={bannerpicture} src="./GDCantiklanbanner.jpg"/>
         <h4 className="leftaligned">General Defense Committee</h4>
         <p>The Industrial Workers of the World (IWW)'s General Defense Committee (GDC) was formed to defend working class people fighting for their rights on the picket line and in their community.</p>
         <div className="leftaligned">
-          <p>The GDC aims include:</p>
-          <ul>
-            <li>Providing support to any member of the working class who finds themselves in legal trouble due to their involvement in the class war.</li>
-            <li>Organizing against forces of oppression that divide the working class, including colonialism, fascism, heterosexism, racism, patriarchy, state repression, transphobia, and xenophobia, among others.</li>
-            <li>Building networks for community-based self-defense.</li>
-          </ul>
-          <img className="imgtest" alt="General Defense Comittee Logo" style={gdclogo} src="./GDCbanner.jpeg"/>
-          <p>The Raleigh-Durham GDC's projects currently include:</p>
-          <ul>
-            <li>Organizing to counter the resurgent movements of white supremacists, neo-Confederates, and the  'alt-right'</li>
-            <li>Community-based food distribution (with Workers Feeding Workers)</li>
-            <li>Campaigns for building renters' power against landlords</li>
-            <li>Organizing to defend people from state and right-wing repression</li>
-          </ul>
+          <div className="row">
+            <div className="col">
+              <p>The GDC aims include:</p>
+              <ul>
+                <li>Providing support to any member of the working class who finds themselves in legal trouble due to their involvement in the class war.</li>
+                <li>Organizing against forces of oppression that divide the working class, including colonialism, fascism, heterosexism, racism, patriarchy, state repression, transphobia, and xenophobia, among others.</li>
+                <li>Building networks for community-based self-defense.</li>
+              </ul>
+            </div>
+            <div className="imgtest">
+              <img alt="General Defense Comittee Logo" style={gdclogo} src="./GDCbanner.jpeg"/>
+            </div>
+          </div>
+          <div className="row">
+            <p style={blocked}>The Raleigh-Durham GDC's projects currently include:</p>
+            <ul>
+              <li>Organizing to counter the resurgent movements of white supremacists, neo-Confederates, and the  'alt-right'</li>
+              <li>Community-based food distribution (with Workers Feeding Workers)</li>
+              <li>Campaigns for building renters' power against landlords</li>
+              <li>Organizing to defend people from state and right-wing repression</li>
+            </ul>
+          </div>
         </div>
         <p>
         Are you interested in getting involved with the GDC?<br/>
