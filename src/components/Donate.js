179089f8031f52fdd5944c8a34bc54c2539089b1
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-12-02 12:45:23 -0500

    add link to paypal

--- src/components/Donate.js
+++ src/components/Donate.js
@@ -11,6 +11,8 @@ class Donate extends Component {
     return (
       <div className="Donate component">
         <h4 className="leftaligned">Want to donate? <NavLink to="/contact">Sign Up</NavLink> for the IWW!</h4>
+        <p>The best way to donate to our branch is to join us and donate not just money, but your time, working with us to organize!</p>
+        <p>However, if you really want to donate money, you can donate via our <a href="https://www.paypal.me/rdiwwgmb" target="_blank" rel="noopener noreferrer">paypal here</a></p>
       </div>
     );
   }
