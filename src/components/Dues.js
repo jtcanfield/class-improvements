984fa2bf4f74dd1588deb2fd632ce4d2907a0cce
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-12-03 14:44:19 -0500

    format dues data

--- src/components/Dues.js
+++ src/components/Dues.js
@@ -7,8 +7,10 @@ class Dues extends Component {
     }
   }
   render() {
+    let fourmready = true;
     return (
       <div className="Dues component">
+        <br/>
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick"/>
           <input type="hidden" name="hosted_button_id" value="LHL3TDRYT7PWW"/>
@@ -24,7 +26,7 @@ class Dues extends Component {
           Please enter your first and last name<br/><br/>
           <input type="text" name="os1" maxLength="200"/><br/><br/>
           <input type="hidden" name="currency_code" value="USD"/>
-          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" disabled={!fourmready}/>
           <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
         </form>
       </div>
