50e2516912b3cbd2e3a3e7f51359cfb89e5fced2
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-12-03 14:32:42 -0500

    edit text

--- src/components/Dues.js
+++ src/components/Dues.js
@@ -12,7 +12,7 @@ class Dues extends Component {
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick"/>
           <input type="hidden" name="hosted_button_id" value="LHL3TDRYT7PWW"/>
-          <input type="hidden" name="on0" value="Income Per Month Dues"/>
+          <input type="hidden" name="on0" value="Income Per Month --- Dues"/>
           Income Per Month --- Dues<br/><br/>
           <select name="os0">
           	<option value="Unemployed">Unemployed --- $6.00 USD</option>
@@ -20,8 +20,8 @@ class Dues extends Component {
           	<option value="$2,000 – $3,500/mo">$2,000 - $3,500/mo --- $22.00 USD</option>
           	<option value="Over $3,500/mo">Over $3,500/mo --- $33.00 USD</option>
           </select><br/><br/>
-          <input type="hidden" name="on1" value="Please enter your name"/>
-          Please enter your name<br/><br/>
+          <input type="hidden" name="on1" value="Please enter your first and last name"/>
+          Please enter your first and last name<br/><br/>
           <input type="text" name="os1" maxLength="200"/><br/><br/>
           <input type="hidden" name="currency_code" value="USD"/>
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
