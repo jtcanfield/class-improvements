e971544986bca9cb7fb1c5354196b8e587aa70dc
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-12-07 21:29:23 -0500

    fix issue with dues payment

--- src/components/Dues.js
+++ src/components/Dues.js
@@ -14,6 +14,25 @@ class Dues extends Component {
         <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
           <input type="hidden" name="cmd" value="_s-xclick"/>
           <input type="hidden" name="hosted_button_id" value="LHL3TDRYT7PWW"/>
+          <input type="hidden" name="on0" value="Income Per Month Dues"/>
+          Income Per Month --- Dues<br/><br/>
+          <select name="os0">
+          	<option value="Unemployed ---">Unemployed --- $6.00 USD</option>
+          	<option value="Under $2,000/mo ---">Under $2,000/mo --- $11.00 USD</option>
+          	<option value="$2,000 - $3,500/mo ---">$2,000 - $3,500/mo --- $22.00 USD</option>
+          	<option value="Over $3,500/mo ---">Over $3,500/mo --- $33.00 USD</option>
+          </select><br/><br/>
+          <input type="hidden" name="on1" value="Enter your first and last name"/>
+          Enter your first and last name<br/><br/>
+          <input type="text" name="os1" maxlength="200"/><br/><br/>
+          <input type="hidden" name="currency_code" value="USD"/>
+          <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!"/>
+          <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
+        </form><br/><br/>
+        Payments made securely and anonymously through PayPal
+        {/*<form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+          <input type="hidden" name="cmd" value="_s-xclick"/>
+          <input type="hidden" name="hosted_button_id" value="LHL3TDRYT7PWW"/>
           <input type="hidden" name="on0" value="Income Per Month --- Dues"/>
           Income Per Month --- Dues<br/><br/>
           <select name="os0">
@@ -28,7 +47,7 @@ class Dues extends Component {
           <input type="hidden" name="currency_code" value="USD"/>
           <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_paynow_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!" disabled={!fourmready}/>
           <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"/>
-        </form>
+        </form>*/}
       </div>
     );
   }
