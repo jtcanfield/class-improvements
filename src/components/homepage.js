dcf111d46508b4905c51750f40abc999682a2644
Jonathan Canfield <jonathantcanfield@gmail.com>
2018-01-03 07:46:18 -0500

    add links to articles

--- src/components/homepage.js
+++ src/components/homepage.js
@@ -37,6 +37,57 @@ class Homepage extends Component {
     return (
       <div className="Homepage component">
         <h4 className="leftaligned">Recent News</h4>
+        <div>
+          <p>
+            <a href="LINKHERE" target="_blank" rel="noopener noreferrer">
+            TITLEHERE
+            </a><br/>
+            Its Going Down - February 15, 2016
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/igd-durham-community-quickly-mobilizes-defeat-klan/" target="_blank" rel="noopener noreferrer">
+            IGD Durham: Community Quickly Mobilizes to Defeat Klan
+            </a><br/>
+            Its Going Down - August 19, 2017
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/durham-community-tears-down-statue-in-solidarity-with-cville/" target="_blank" rel="noopener noreferrer">
+            Durham Tears Down Confederate Statue in Solidarity with Cville
+            </a><br/>
+            Its Going Down - August 15, 2017
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/raleigh-durham-nc-iwoc-friends-picket-nc-department-public-safety/" target="_blank" rel="noopener noreferrer">
+            Raleigh-Durham, NC: IWOC and Friends Picket NC Department of ‘Public Safety’
+            </a><br/>
+            Its Going Down - June 26, 2017
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/raleigh-nc-report-back-united-racism-islamophobia/" target="_blank" rel="noopener noreferrer">
+            Raleigh, NC: Report-Back from “United Against Racism and Islamophobia”
+            </a><br/>
+            Its Going Down - June 11, 2017
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/call-campaign-iwoc-member-stanley-corbett-jr/" target="_blank" rel="noopener noreferrer">
+            Call-In Campaign for IWOC Member Stanley Corbett Jr.
+            </a><br/>
+            Its Going Down - March 20, 2017
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/durham-iww-stands-striking-prisoners/" target="_blank" rel="noopener noreferrer">
+            Durham: IWW stands with Striking Prisoners!
+            </a><br/>
+            Its Going Down - August 11, 2016
+          </p>
+          <p>
+            <a href="https://itsgoingdown.org/final-straw-inside-outside-alliance-deaths-durham-county-jail-nc/" target="_blank" rel="noopener noreferrer">
+            Final Straw: Inside Outside Alliance on deaths in Durham County Jail, NC
+            </a><br/>
+            Its Going Down - February 15, 2016
+          </p>
+        </div>
+        <h4 className="leftaligned">Facebook Feed</h4>
         <iframe title="news" src={iframelink}
         style={iframestyles}></iframe>
       </div>
